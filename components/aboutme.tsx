43ea9e5393bf7ceee4eefe17144157cf863bbff4
keirastanley <keiraestanley@gmail.com>
2023-01-23 20:51:48 +0000

    Apostrophes replaced with universal html symbol to fix deployment error

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -2,6 +2,6 @@ import styles from "../styles/aboutme.module.css"
 
 export default function AboutMe() {
     return <div className={styles.aboutme_container}>
-        <div className={styles.aboutme_text}><p>Hi, I`m Keira.</p><br></br><p>I started learning to code on a whim in January 2021. Before that, my work was centred around foreign languages and I spent most of my free time making music, painting and playing chess. Coming from a non-STEM background, I'd assumed I'd be poorly suited to coding but the skills I had honed from my hobbies and interests turned out to be invaluable when learning to code, from pattern recognition and attention to detail, to logical analysis and creative thinking.</p></div>
+        <div className={styles.aboutme_text}><p>Hi, I&#39m Keira.</p><br></br><p>I started learning to code on a whim in January 2021. Before that, my work was centred around foreign languages and I spent most of my free time making music, painting and playing chess. Coming from a non-STEM background, I&#39d assumed I&#39d be poorly suited to coding but the skills I had honed from my hobbies and interests turned out to be invaluable when learning to code, from pattern recognition and attention to detail, to logical analysis and creative thinking.</p></div>
 </div>
 }
