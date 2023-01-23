d91b6b67572bcd176cf643c07b3d105f14f710a6
keirastanley <keiraestanley@gmail.com>
2023-01-23 23:42:04 +0000

    Change to about me page

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -2,6 +2,14 @@ import styles from "../styles/aboutme.module.css"
 
 export default function AboutMe() {
     return <div className={styles.aboutme_container}>
-        <div className={styles.aboutme_text}><p>Hi, I&#39;m Keira.</p><br></br><p>I started learning to code on a whim in January 2021. Before that, my work was centred around foreign languages and I spent most of my free time making music, painting and playing chess. Coming from a non-STEM background, I&#39;d assumed I&#39;d be poorly suited to coding but the skills I had honed from my hobbies and interests turned out to be invaluable when learning to code, from pattern recognition and attention to detail, to logical analysis and creative thinking.</p></div>
+        <div className={styles.aboutme_text}>
+            <p>Hi, I&#39;m Keira. As a junior full-stack developer, I have a solid understanding of Javascript, HTML, CSS, Node.js, and Express, as well as a unique background and set of skills that sets me apart from others in the field.
+            <br></br>
+            My greatest passion in life has always been music. Since I was a young child, I've been playing the piano and have been composing and producing music for many years. This passion has given me an appreciation for the similarities between creating structured patterns and sequences of information in music and coding. Both require attention to detail, logical thinking and problem-solving, whether it's figuring out how to handle a clash of sonic frequencies or how to fix a bug in a program.
+            <br></br>
+            In addition to music, I am also passionate about languages and translation, which I studied at university. After graduation, I moved to Spain, where I lived for four years working as a language instructor. This experience has helped me to understand the similarities between translating English to Spanish and translating human language to code. My ability to adapt my speech and break complex information into simplified concepts has been invaluable when collaborating with other programmers, especially when explaining problems or difficult concepts.
+            <br></br>
+            Overall, I'm a versatile and skilled developer who is able to draw on my diverse background to create innovative and effective solutions.</p>
+        </div>
 </div>
 }
