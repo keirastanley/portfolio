e90228b41e88d05574043a2b586ce323f98511ae
keirastanley <keiraestanley@gmail.com>
2023-01-24 00:59:22 +0000

    Apostrophe error fixed again

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -5,11 +5,11 @@ export default function AboutMe() {
         <div className={styles.aboutme_text}>
             <p>Hi, I&#39;m Keira. As a junior full-stack developer, I have a solid understanding of Javascript, HTML, CSS, Node.js, and Express, as well as a unique background and set of skills that sets me apart from others in the field.
             <br></br>
-            My greatest passion in life has always been music. Since I was a young child, I've been playing the piano and have been composing and producing music for many years. This passion has given me an appreciation for the similarities between creating structured patterns and sequences of information in music and coding. Both require attention to detail, logical thinking and problem-solving, whether it's figuring out how to handle a clash of sonic frequencies or how to fix a bug in a program.
+            My greatest passion in life has always been music. Since I was a young child, I&#39;ve been playing the piano and have been composing and producing music for many years. This passion has given me an appreciation for the similarities between creating structured patterns and sequences of information in music and coding. Both require attention to detail, logical thinking and problem-solving, whether it&#39;s figuring out how to handle a clash of sonic frequencies or how to fix a bug in a program.
             <br></br>
             In addition to music, I am also passionate about languages and translation, which I studied at university. After graduation, I moved to Spain, where I lived for four years working as a language instructor. This experience has helped me to understand the similarities between translating English to Spanish and translating human language to code. My ability to adapt my speech and break complex information into simplified concepts has been invaluable when collaborating with other programmers, especially when explaining problems or difficult concepts.
             <br></br>
-            Overall, I'm a versatile and skilled developer who is able to draw on my diverse background to create innovative and effective solutions.</p>
+            Overall, I&#39;m a versatile and skilled developer who is able to draw on my diverse background to create innovative and effective solutions.</p>
         </div>
 </div>
 }
